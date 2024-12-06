d6a521ca8b14556a4264dc60df180bb9e0fa823f
Francesco-Pagano <frank.pagano@protonmail.ch>
2024-12-06 18:57:43 +0100

    Green phase US3.1

--- src/cleaning_robot.py
+++ src/cleaning_robot.py
@@ -73,8 +73,9 @@ class CleaningRobot:
         return f"({self.pos_x},{self.pos_y},{self.heading})"
 
     def execute_command(self, command: str) -> str:
-        # To be implemented
-        pass
+        if command == self.FORWARD:
+            self.pos_y += 1
+
 
     def obstacle_found(self) -> bool:
         # To be implemented
